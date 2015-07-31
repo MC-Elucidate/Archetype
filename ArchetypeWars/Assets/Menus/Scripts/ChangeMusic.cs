399d61b1ba60ceecd8835759019b49fdc5c1fce8
axionicm4tt3r <marcelroodt@gmail.com>
2015-08-01 01:14:04 +0200

    Menu First Commit
    Added Assets, new Scripts.
    
    Menus are added, but don't go anywhere yet. High Score and Options are
    incomplete. Sounds are working as planned.

---
+++ ArchetypeWars/Assets/Menus/Scripts/ChangeMusic.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeMusic : MonoBehaviour {
+
+	public AudioClip menuBGM;
+	public AudioClip charSelectBGM;
+
+	private AudioSource player;
+
+	// Use this for initialization
+	void Start () {
+
+		player = GetComponent<AudioSource> ();
+		player.clip = menuBGM;
+		player.Play ();
+		player.loop = true;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void switchSong() {
+
+		if (player.clip == menuBGM) {
+			player.Stop();
+			player.clip = charSelectBGM;
+			player.volume = 0.8f;
+			player.Play ();
+			player.loop = true;
+		}
+
+		else if (player.clip == charSelectBGM) {
+			player.Stop();
+			player.clip = menuBGM;
+			player.volume = 1f;
+			player.Play ();
+			player.loop = true;
+		}
+	}
+
+}
